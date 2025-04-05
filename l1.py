0e8e9a810c36bce652aacf731b0381921dedcb84
Sten Heimbrodt <39519505+gespel@users.noreply.github.com>
2025-04-05 08:51:24 +0200

    day 1

---
+++ l1.py
@@ -0,0 +1,18 @@
+from torch import nn
+
+class TestNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.layers = nn.Sequential(
+            nn.Linear(2, 32),
+            nn.ReLU(),
+            nn.Linear(32,32),
+            nn.ReLU(),
+            nn.Linear(32, 1)
+        )
+
+    def forward(self, x):
+        return self.layers(x)
+
+m = TestNN()
