e27c597cf56865d499fd3ceab56ed0ca39c7c75c
sheimbrodt <heimbrodt@uni-potsdam.de>
2025-04-08 14:46:18 +0200

    puuh

---
+++ l7.py
@@ -0,0 +1,19 @@
+from torch import nn
+
+class TestNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.layers = nn.Sequential(
+            nn.Linear(2, 32),
+            nn.ReLU(),
+            nn.Linear(32, 32),
+            nn.ReLU(),
+            nn.Linear(32, 1)
+        )
+
+    def forward(x):
+        return self.layers(x)
+    
+m = TestNN()
+print(m)
